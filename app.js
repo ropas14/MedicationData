bb875e6fc68a578e7fa8053b902e9de4fac20192
ropas14 <37495035+ropas14@users.noreply.github.com>
2018-06-21 18:34:12 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -19,80 +19,50 @@ app.engine('hbs', hbs({extname:'hbs' , defaultLayout:'layout',layoutsDir:__dirna
 app.set('view engine','hbs');
 
 app.get('/api/medication',function(req,res){
-	var meds = req.query.search.trim().toLowerCase();
-	if(meds.includes("%20")){
-	  meds=meds.replace("%20"," ");
-	}
-	else{meds;}
-	
+	var meds = req.query.search.trim().toLowerCase();	
     mongodb.connect(mongourl, function(err, db) {
           if (err) throw err;
           var drugsResults=[];  
           var dbo = db.db("scrapesites");
-          dbo.collection("drugsGoodrx").createIndex({ drug: "text" });
-          var query =  { $text: { $search: meds } };        
+         // dbo.collection("drugsGoodrx").createIndex({ drug: "text" });  
+          var query =  { drug:new RegExp('^' + meds)} ;        
              dbo.collection("drugsGoodrx").find(query).toArray(function(error, result) {
                if (error) throw error;
-               var output= reverse(result);
-               output.forEach(function(drug){
-                drugsResults.push(drug);  
-               });
-           
-        
-        var drugitem={};
-        var drgdist=[];
-        var drugnames = [];
-         dbo.collection("drugsGoodrx").find().toArray(function(err,rslt){
-            if(err) throw err;
-            rslt.forEach(function(data){
-            var distance = levenshtein.get(meds, data.drug, { useCollator: true});
-            // drug distance as key , drug as value
-            drugitem[distance] = data.drug;
-            //push leven distance in an array
-            drgdist.push(distance);
-            //push they  distance and drug object in drugnames array
-            drugnames.push(drugitem);
-            drugitem = {}; 	
-            }); 
-            // sort distances in ascending order
-            drgdist.sort(sortNumber);
-            //assign closestdistance which is not=0, by default drgdist[0] is equal to zero
-            var closedistance= drgdist[1];  
-            //loop in the array of drugnames objects          
-            	drugnames.forEach(function(dg){
-                 var Keys = Object.keys(dg);
-                 //if closestdistance is equal to the object key enter loop
-                 if(closedistance.toString()===Keys[0]){
-                      var name = dg[Keys[0]];
-                      console.log("----------------------------------- " + name);
-                     dbo.collection("drugsGoodrx").find({drug:name}).toArray(function(errr, rsult) {
-                     if (errr) throw errr;   
-                      console.log(rsult.pop()); 
-                     //push object result in the drugsResults array           
-                   	  drugsResults.push(rsult.pop());                                     	   
-                      });
-                     
-                 }              
-            	});
-           console.log(drugsResults);  
-           res.render('main',drugsResults);    
-          });       
-      });               
-      
-     });
-});
+              // res.render('main',result);
+               var drugnamez=[];
+               result.forEach(function(row){
+                drugnamez.push(row.drug);
+                var distance = levenshtein.get(meds, row.drug, { useCollator: true});
+                row.distance= distance;
 
-function reverse(a) {
-     var temp = [];
-     var len = a.length;
-     for (var i = (len-1); i>=0; i--) {
-        temp.push(a[i]);
-     }
-     return temp;
-}
+               });
+                
+              console.log(drugnamez)
+               query = {$text: { $search: meds },drug:{$nin:drugnamez}}
+                dbo.collection("drugsGoodrx").find(query).toArray(function(err, reslt){
+                  if (err) throw err;
+                reslt.forEach(function(row){
+                 drugnamez.push(row.drug);
+                 var distance = levenshtein.get(meds, row.drug, { useCollator: true});
+                 row.distance= distance;
+
+               });
+                    reslt=reslt.sort(sortNumber);
+                    result=result.sort(sortNumber);
+                    result=result.concat(reslt);
+                    res.render('main',result);
+                });
+
+            });    
 
 function sortNumber(a,b) {
-    return a - b;
+  var dist= parseInt(a.distance) - parseInt(b.distance);
+  if(dist == 0){
+     return parseInt(a.popularity) - parse(b.popularity);
+  }else{
+       return dist;
+  }
+
 }
 
 app.listen(app.get('port'));
